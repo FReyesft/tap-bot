63675970b289c4c0b542565ff7ce9f26069b1f00
FReyeSFT <f.reyesft@gmail.com>
2024-12-02 17:31:10 -0500

    add first command

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const discord_js_1 = require("discord.js");
-const ready_1 = tslib_1.__importDefault(require("./listerners/ready"));
 const environment_config_1 = require("./config/environment-config");
-const token = environment_config_1.config.BOT_TOKEN;
-const client = new discord_js_1.Client({
-    intents: []
+const chalk_1 = tslib_1.__importDefault(require("chalk"));
+const client = new discord_js_1.Client({ intents: [discord_js_1.GatewayIntentBits.Guilds] });
+client.once(discord_js_1.Events.ClientReady, readyClient => {
+    console.log(chalk_1.default.bgRed.bold(`Ready! Logged in as ${readyClient.user.tag}`));
 });
-(0, ready_1.default)(client);
-client.login(token);
+client.login(environment_config_1.config.BOT_TOKEN);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
-import { Client } from "discord.js";
-import ready from "./listerners/ready";
-import { config } from "./config/environment-config";
+import { Client, Events, GatewayIntentBits, CommandInteraction, Interaction } from 'discord.js';
+import { config } from './config/environment-config';
+import { getCommands } from './commands/commands';
+import chalk from 'chalk';
 
-const token = config.BOT_TOKEN;
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const client = new Client({
-    intents: []
+const registerCommands = async (client: Client) => {
+    try {
+        const commands = getCommands();
+        for (const cmd of commands) {
+            await client.application?.commands.create(cmd.data.toJSON());
+            console.log(chalk.bgBlue(`Comando registrado: ${cmd.data.name}`));
+        }
+    } catch (error) {
+        console.error(chalk.bgRed("Error al registrar los comandos: "), error);
+    }
+};
+
+client.on(Events.InteractionCreate, async (interaction: Interaction) => {
+    if (interaction.isCommand()) {
+        const { commandName } = interaction as CommandInteraction;
+
+        const commands = getCommands();
+        const command = commands.find(cmd => cmd.data.name === commandName);
+        if (command) {
+            await command.execute(interaction);
+        }
+    }
 });
 
-ready(client);
+client.once(Events.ClientReady, async () => {
+    console.log(chalk.bgGreen.bold(`Ready! Logged in as ${client.user?.tag}`));
+    
+    await registerCommands(client);
+});
 
-client.login(token);
+client.login(config.BOT_TOKEN);
