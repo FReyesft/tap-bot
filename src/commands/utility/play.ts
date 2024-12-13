501a7dac8b8055d5979d0506575a40e34992ddb3
FReyeSFT <f.reyesft@gmail.com>
2024-12-13 09:20:39 -0500

    (add): command to enter in a channel voice

--- src/commands/utility/play.ts
+++ src/commands/utility/play.ts
@@ -1,33 +1,32 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder, VoiceChannel } from "discord.js";
-import { joinVoiceChannel, VoiceConnectionStatus, entersState, DiscordGatewayAdapterImplementerMethods, DiscordGatewayAdapterLibraryMethods } from "@discordjs/voice";
-import { config } from "src/config/environment-config";
+import { entersState, joinVoiceChannel, VoiceConnectionStatus } from "@discordjs/voice";
+import { ChatInputCommandInteraction, SlashCommandBuilder, GuildMember } from "discord.js";
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
-		.setDescription('Reproduce la canción que desees en un canal de voz')
+		.setDescription('Hace que el bot se una a tu canal de voz y reproduzca música')
 		.addStringOption(option =>
 			option.setName('name')
-				.setDescription('Nombre de la canción')
-				.setRequired(true))
-		.addChannelOption(option =>
-			option
-				.setName('channel')
-				.setDescription('El canal de voz donde deseas reproducir')
-				.addChannelTypes(2)
+				.setDescription('Nombre o url de la canción')
 				.setRequired(true)),
 	async execute(interaction: ChatInputCommandInteraction) {
-		const channel = interaction.options.getChannel('channel') as VoiceChannel;
-		
-		try {
-			const songName = interaction.options.getString('name') ?? ''
-			await interaction.reply('Reproduciendo: ' + songName)
-		} catch (error) {
-			console.error(error);
-			await interaction.reply({
-				content: "No se pudo conectar al canal de voz. Verifica los permisos del bot.",
-				ephemeral: true,
-			});
+		const member = interaction.member as GuildMember;
+		const songName = interaction.options.getString('name') ?? 'No se proporciono un valor'
+		if (!member.voice.channel) {
+			return interaction.reply('¡Debes estar en un canal de voz para que el bot se una!');
 		}
-	}
+
+		joinVoiceChannel({
+			channelId: member.voice.channel.id,
+			guildId: interaction.guildId!,
+			adapterCreator: interaction.guild!.voiceAdapterCreator,
+		});
+
+		try {
+			interaction.reply(`Reproduciendo: ${songName}`);
+		} catch (error) {
+			console.error('Error al unirse al canal de voz:', error);
+			interaction.reply('Hubo un error al intentar unirme al canal de voz.');
+		}
+	},
 };
